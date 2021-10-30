0b32b6a0db977a533f4b65eb3a793400a9fd1603
Valerij Ivashchenko <likemusicdev@gmail.com>
2021-10-30 23:22:28 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import parse from 'date-format-parse';
+
+
+function getMatchedByLengthFormats(str, formats) {
+    const strLength = str.length;
+
+    const matched = formats.filter((format) => strLength <= format.length);
+
+    matched.sort();
+
+    return matched;
+}
+
+export function parseMultiFormats(
+    str,
+    formats,
+    options = {}
+) {
+    str = str.trim();
+
+    const matchedByLengthFormats = getMatchedByLengthFormats(str, formats);
+
+    let result = new Date(NaN);
+
+    for (let format of matchedByLengthFormats) {
+        result = parse(str, format, options);
+
+        if(!isNaN(result.valueOf())) {
+            return result;
+        }
+    }
+
+    return result;
+}
