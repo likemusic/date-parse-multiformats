68236e62323b1c10f6991d283603963fa8a7a5c6
Valerij Ivashchenko <likemusicdev@gmail.com>
2021-10-31 22:12:27 +0300

    Fixes in index.js.

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
 import {parse} from 'date-format-parse';
 
-
-function getMatchedByLengthFormats(str, formats) {
-    const strLength = str.length;
-
-    const matched = formats.filter((format) => strLength <= format.length);
-
-    matched.sort();
-
-    return matched;
-}
-
 export default function parseMultiFormats(
     str,
     formats,
     options = {}
 ) {
-    str = str.trim();
-
-    const matchedByLengthFormats = getMatchedByLengthFormats(str, formats);
-
     let result = new Date(NaN);
 
-    for (let format of matchedByLengthFormats) {
+    for (let format of formats) {
         result = parse(str, format, options);
 
         if(!isNaN(result.valueOf())) {
